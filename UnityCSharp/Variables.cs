96bf3c9c075262a8c5ed9cd5a756ea7bc14fff46
rosiehighsmith <rosie.highsmith@gmail.com>
2017-05-19 07:42:18 -0700

    Initial commit

---
+++ UnityCSharp/Variables.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnityCSharp
+{
+    class Variables
+    {
+        public string name = "Rosie";
+        private int age = 27;
+        private string gender = "Female";
+        private string location = "San Diego";
+        public bool isAwesome = true;
+        private float speed = 55.5f;
+    }
+}
